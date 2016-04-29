8b50c8e79053b0ba75f6d51c7e360805321e7883
Olivier Poulin <poulio445@gmail.com>
2016-04-29 00:54:44 -0400

    lab9

--- checkpoint4.py
+++ checkpoint4.py
@@ -2,4 +2,11 @@ from pymongo import MongoClient
 client = MongoClient()
 
 if __name__ == '__main__':
-    print "Modify me"
+   db = client.csci2963
+   posts = db.definitions
+   print posts.find()
+   print posts.find_one() 
+   print posts.find({"word": "Capitalandi"})
+   print posts.find({"_id": "ObjectId('56fe9e22bad6b23cde07b8ce')"})
+   post = {'definition': " exp. To laugh out loud.", 'word': 'lol'}
+   posts.insert_one(post)
--- checkpoint5.py
+++ checkpoint5.py
@@ -1,14 +1,24 @@
 from pymongo import MongoClient
+import random
+import time
+import datetime
 client = MongoClient()
 
 
 def random_word_requester():
-    '''
-    This function should return a random word and its definition and also
-    log in the MongoDB database the timestamp that it was accessed.
-    '''
-    return
+	'''
+	This function should return a random word and its definition and also
+	log in the MongoDB database the timestamp that it was accessed.
+	'''
+	db = client.csci2963
+	posts = db.definitions
+	size = len(list(posts.find()))
+	i = random.randint(0, size-1)
+	word = list(posts.find())[i]["word"]
+	timestamp = str(datetime.datetime.fromtimestamp(time.time()))
+	posts.update({"word":word}, {"$push":{"dates":timestamp}})
+	return str(list(posts.find())[i])
 
 
 if __name__ == '__main__':
-    print random_word_requester()
+	print random_word_requester()
